d9e70f46092b18bcedd7765fa44c1c08f0089add
Hector Perez <hecpeare@gmail.com>
2020-10-12 12:49:14 +0200

    fix seeds - replace topics with notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 
 help_user = User.create!(name: "Help", email: "help@help.org", username: "help", password: "help183A", password_confirmation: "help183A")
 help_user.confirm
-t1 = help_user.topics.create!(content: "Welcome")
-t1.children.create!(content: "Welcome to the Book Notes Club!", user: help_user)
+note1 = help_user.notes.create!(content: "Welcome")
+note1.children.create!(content: "Welcome to the Book Notes Club!", user: help_user)
 
 user = User.create!(name: "Marie Curie", email: "marie@curie.com", username: "curie", password: "mariecurie", password_confirmation: "mariecurie")
 user.confirm
