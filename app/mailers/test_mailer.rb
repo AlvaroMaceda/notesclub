08248ef20944ce27e7dd9e554c11c9b6d1a4669b
Hector Perez <hecpeare@gmail.com>
2020-10-15 18:06:57 +0200

    test mailer

---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,8 @@
+# How to use it:
+# TestMailer.with(user: user).test_email.deliver_now
+class TestMailer < ApplicationMailer
+  def test_email
+    @user = params[:user]
+    mail(to: @user.email, subject: "Test email")
+  end
+end
