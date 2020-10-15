9a9592b2b264b93e1d704bc1f4f171b0327e6bb3
Hector Perez <hecpeare@gmail.com>
2020-10-15 18:06:50 +0200

    AWS SES initializer

---
+++ config/initializers/aws.rb
@@ -0,0 +1,7 @@
+creds = Aws::Credentials.new(ENV['AWS_SES_ACCESS_KEY_ID'], ENV['AWS_SES_SECRET_ACCESS_KEY'])
+
+Aws::Rails.add_action_mailer_delivery_method(
+  :ses,
+  credentials: creds,
+  region: 'eu-central-1'
+)
