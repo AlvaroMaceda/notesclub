23e8373deef3f93335c8c2841796174f438310b5
Hector Perez <hecpeare@gmail.com>
2020-10-23 19:25:56 +0200

    change to content · @username

--- front/src/notes/Item.js
+++ front/src/notes/Item.js
@@ -1,3 +1,3 @@
 import * as React from 'react';
 
-export const Item = ({ entity: { username, content } }) => <div>{`${username}: ${content}`}</div>;
+export const Item = ({ entity: { username, content } }) => <div>{`${content} · @${username}`}</div>;
