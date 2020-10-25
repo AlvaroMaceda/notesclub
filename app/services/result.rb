ae04b652af8ea0e00e285cd2daa9ebf6fd1f771d
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-10-25 17:54:53 +0100

    Adds a Result object

---
+++ app/services/result.rb
@@ -0,0 +1,33 @@
+class Result
+
+  class << self
+
+    def ok(value = nil)
+      self.new success: true, value: value.clone.freeze, errors: nil
+    end
+
+    def error(errors = nil)
+      self.new success: false, value: nil, errors: errors.clone.freeze
+    end
+
+  end
+
+  attr_reader :value, :errors
+
+  def success?
+    @success
+  end  
+
+  private
+
+  class << self
+    protected :new
+  end
+
+  def initialize(**values)
+    @success = !!values[:success]
+    @value = values[:value]
+    @errors = values[:errors]
+  end
+
+end
