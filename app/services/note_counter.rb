55c2f2b967c0869bc5ceef306e883c7556f757c2
Alvaro Maceda <alvaro@alvaromaceda.es>
2020-10-30 07:11:58 +0100

    Changes a number for corresponding constant in NoteCounter

--- app/services/note_counter.rb
+++ app/services/note_counter.rb
@@ -20,14 +20,14 @@ class NoteCounter < ApplicationService
   def non_root_notes(url)
     Note.
       where("lower(content) like ?", "%#{url}%").
-      where("ancestry is not null").limit(10).count
+      where("ancestry is not null").limit(MAX_COUNT).count
   end
 
   def non_empty_root_notes(url)
     Note.
       joins("inner join notes as t on t.ancestry = cast(notes.id as VARCHAR(255)) and t.position=1 and t.content != ''").
       where("notes.ancestry is null").
-      where("lower(notes.content) like ?", "%#{url}%").limit(10).count
+      where("lower(notes.content) like ?", "%#{url}%").limit(MAX_COUNT).count
   end
 
 end
